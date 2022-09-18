1de77fe1ee87a301d48bdc2099d679c959c8ce77
pierre Edimo <pedimonkoe@yahoo.com>
2022-09-18 14:38:43 +0200

    add role in to the authentication and add Authorize attribute

--- Model/User.cs
+++ Model/User.cs
@@ -11,5 +11,7 @@ namespace user_service.Model
         public String? City { get; set; }
         public String? Country { get; set; }
         public String? HouseNumber { get; set; }
+        public String? RoleId{get; set;}
+        public virtual Role? Role{get; set;}
     }
 }
