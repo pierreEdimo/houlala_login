db222a7a19ec0c039766e2a76a6d0625fa048902
pierre Edimo <pedimonkoe@yahoo.com>
2022-12-31 03:08:09 +0100

    creer le LoginException

---
+++ exception/LoginException.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace user_service.exception;
+
+public class LoginException : Exception
+{
+    public int Status { get; set; }
+    public string? TimeStamp { get; set; }
+    public IEnumerable<IdentityError>? Errors { get; set; }
+
+    public LoginException()
+    {
+    }
+
+    public LoginException(string message, int status) : base(message)
+    {
+        Status = status;
+    }
+
+    public LoginException(string message, Exception innerException, int status) : base(message, innerException)
+    {
+        Status = status;
+    }
+
+    public LoginException(IEnumerable<IdentityError> errors, int status)
+    {
+        Status = status;
+        Errors = errors;
+    }
+}
