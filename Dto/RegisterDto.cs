77dfe30e26c4a68a315bccc721fa08bdd95fa423
pnk <pierre.nkoe@vema-eg.de>
2025-01-06 16:02:37 +0100

    Refactoring du RegisterDto.

--- Dto/RegisterDto.cs
+++ Dto/RegisterDto.cs
@@ -1,15 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace user_service.Dto;
 
 public class RegisterDto
 {
-    [Required] public string? UserName { get; set; }
-    [Required] public string? PassWord { get; set; }
-    [Required] public string? Email { get; set; }
-    public string? LastName { get; set; }
-    public string? FirstName { get; set; }
-    public string? PhoneNumber { get; set; }
-
-    public string? RoleId { get; set; }
+    public string? Email { get; set; }
+    public string? UserName { get; set; }
+    public string? PassWord { get; set; }
 }
