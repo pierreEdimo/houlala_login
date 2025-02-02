88d33197a4347faea8a279d8a382521553554c0b
pnk <pierre.nkoe@vema-eg.de>
2025-02-02 15:09:17 +0100

    migration pour l'extension des adresses.

---
+++ Migrations/20250202140727_address_extension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace user_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class address_extension : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Addresses",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Addresses",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Addresses");
+        }
+    }
+}
