fe1bb1b5ad74dfdc437adae4673093abf6c4d0e3
pierreedimo <pedimonkoe@yahoo.com>
2025-06-15 17:39:38 +0200

    config

--- obj/Debug/net9.0/user-service.AssemblyInfo.cs
+++ obj/Debug/net9.0/user-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("user-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d11a7284029640996df51e34f01aa99ea14cb8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256b9f3ccc3dd7c0727c8389d3778bb72b26dbba")]
 [assembly: System.Reflection.AssemblyProductAttribute("user-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("user-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
